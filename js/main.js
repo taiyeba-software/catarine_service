cb0390842c4637fe2dedce0ba6306fe70b79d9e6
taiyeba-software <islamtaiyeba38@gmail.com>
2024-11-20 22:38:04 +0600

    bootstrap show class is not removing

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// after scrolling effect for navbar :header scroll
+
+let nav=document.querySelector(".navbar");
+
+window.onscroll=function(){
+    if(document.documentElement.scrollTop>20)
+    {
+        nav.classList.add("header-scrolled");
+    }
+    else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+//writing a js code for collapsing the nav after clicking nav menu
+
+let navmenu=document.querySelectorAll(".nav-link");
+
+let navCollapse=document.quarySelector(".navbar-collapse.collapse");
+
+navmenu.forEach(function(a) {
+    a.addEventListener("click",function() {
+        
+        navCollapse.classList.remove("show");
+    })    
+})
